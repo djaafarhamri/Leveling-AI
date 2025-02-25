23454213389caac16100940ea89dc14dc1e744b7
djaafarhamri <hamridjaafar12@gmail.com>
2025-02-25 14:22:54 +0100

    trying to exclude files in lint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "commonjs" 
+    } 
+  },
+  { 
+    languageOptions: { 
+      globals: { 
+        ...globals.browser, 
+        ...globals.node // Adds Node.js globals, including `process`
+      } 
+    } 
+  },
+  pluginJs.configs.recommended,
+];
