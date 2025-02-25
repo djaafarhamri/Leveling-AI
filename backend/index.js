c115ef94898c38f7e26f6eb2d4881b338bdf606b
djaafarhamri <hamridjaafar12@gmail.com>
2025-02-25 20:04:07 +0100

    new pipeline

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 // Middleware for parsing JSON
@@ -16,24 +15,11 @@ app.get("/api/test", (req, res) => {
   res.json("fileTree");
 });
 
-// Serve static files from frontend build
-const staticPath = path.join(__dirname, './dist');
-console.log('Static path:', staticPath);
-app.use(express.static(staticPath));
 
-// Handle React routing, return the main index.html file for all routes
-app.get('*', (req, res) => {
-  console.log('Serving index.html for:', req.url);
-  res.sendFile(path.join(staticPath, 'index.html'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// Only run app.listen if not in a Vercel environment
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
 
 // Export app for Vercel
 module.exports = app;
