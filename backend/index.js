ae9a86d9a362c2d4bbaacf8ed1823467c029289e
djaafarhamri <hamridjaafar12@gmail.com>
2025-02-25 14:31:39 +0100

    deploy test

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,7 @@ app.get('*', (req, res) => {
 // Export the Express app for Vercel to use
 module.exports = app
 
-// Start the server locally if not in a Vercel environment
-if (process.env.VERCEL !== '1') {
-  const PORT = process.env.PORT || 3000
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
-}
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
