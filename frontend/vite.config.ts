de9c330ba847e314acf1e3863d699b06d25a0110
djaafarhamri <hamridjaafar12@gmail.com>
2025-02-25 19:21:46 +0100

    removed root npm and deploy each alone

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../backend/dist',
-    emptyOutDir: true, // Clears the folder before building
-  },
-  // Ensure base is set correctly, use '/' if your app is deployed at the root
-  base: '/',
 })
